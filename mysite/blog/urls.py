a08af23e15ae3828c8ce8093d9f5b915d37b0632
santiagobruna <santiagobruna147@gmail.com>
2025-10-28 16:18:15 -0300

    feat: criação do nosso template usando html na prática

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from blog.views.post_view import PostView
+from blog import views
 
 urlpatterns = [
-    path('', PostView.as_view(), name='home')
+    path('', views.PostView.as_view(), name='home'),
+    path("<slug:slug>/", views.PostDetail.as_view(), name="post_detail"),
 ]
